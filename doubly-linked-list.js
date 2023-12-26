22e1d96985f6f843005ecd6aafcb945129fd4dfc
helcio <h.andre@giap.pl>
2023-12-26 13:09:29 +0100

    Complete phase 02

--- doubly-linked-list.js
+++ doubly-linked-list.js
@@ -8,15 +8,37 @@ class DoublyLinkedListNode {
 
 class DoublyLinkedList {
   constructor() {
-    // Your code here
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
 
   addToHead(val) {
-    // Your code here
+    if (!this.head) {
+      this.head = new DoublyLinkedListNode(val);
+      this.tail = this.head;
+      this.length++;
+    } else {
+      let newNode = new DoublyLinkedListNode(val);
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+      this.length++;
+    }
   }
 
   addToTail(val) {
-    // Your code here
+    if (!this.tail) {
+      this.head = new DoublyLinkedListNode(val);
+      this.tail = this.head;
+      this.length++;
+    } else {
+      let newNode = new DoublyLinkedListNode(val);
+      newNode.prev = this.tail;
+      this.tail.next = newNode;
+      this.tail = newNode;
+      this.length++;
+    }
   }
 
   // You can use this function to help debug
